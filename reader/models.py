9d170be23d98dcef502dc912120782095e04d897
Anupama <jasinfoss@gmail.com>
2022-04-17 11:40:09 -0700

    Designed a section and created a model for commenting and all migrations are done.

--- reader/models.py
+++ reader/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from blogger.models import blog
 
-
+class comment(models.Model):
+    blogging=models.ForeignKey(blog,related_name="comments",on_delete=models.CASCADE)
+    Name=models.CharField(max_length=100)
+    Message=models.TextField()
+    Date=models.DateTimeField(auto_now_add=True)
 
--- reader/views.py
+++ reader/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from blogger.models import blog
 
 def details(request):
-    return render(request,'singleimage.html')
+    ID=request.GET['id']
+    blogsobj=blog.objects.get(id=ID)   
+    return render(request,'singleimage.html',{'Blog':blogsobj})
+    
